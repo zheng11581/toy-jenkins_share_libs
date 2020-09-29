273300e399404082485f7e1121cfc85be7ebaea9
zheng11581 <zhenghc@borenhospital.com>
2020-09-29 16:14:18 +0800

    增加GenericWebHook参数

--- vars/maven_release_pipeline.groovy
+++ vars/maven_release_pipeline.groovy
@@ -78,7 +78,7 @@ def call(String giturl, String gitBranch, String serviceName, String artRepoName
             //    echo "${gitlab_token}"
             //    git branch: gitBranch, credentialsId: "${gitlab_token}", url: giturl
             //}
-            git branch: gitBranch, credentialsId: 'gitlab', url: giturl
+            git branch: $ref, credentialsId: 'gitlab', url: $git_url
         }
 
         stage('Env capture') {
